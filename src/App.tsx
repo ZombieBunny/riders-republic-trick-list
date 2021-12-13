cf03d3e2efe2addd5dd9deeb7f4cd67b0b7d2eff
Lee <leevashan.reddi@aax.co.za>
2021-12-13 17:10:41 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import * as tricklist from "./trick-list.json";
+import { Divider, TextField } from "@material-ui/core";
+import Autocomplete from "@mui/material/Autocomplete";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// interface AutocompleteOption {
+//   move: string;
+//   Pc: string;
+//   PlayStation: string;
+//   Xbox: string;
+// }
+interface IState {
+  selected: null | {
+    move: string;
+    Pc: string;
+    Playstation: string;
+    Xbox: string;
+  };
+}
+interface IProps {}
+class App extends React.Component<IProps, IState> {
+  private tricks = JSON.parse(JSON.stringify(tricklist)).default;
+
+  constructor(props: any) {
+    super(props);
+    this.state = { selected: null };
+  }
+  handleChange = (e: any) => {
+    this.setState({ selected: e });
+  };
+
+  render() {
+    let view;
+    if (!!this.state.selected) {
+      view = (
+        <div className="result-container">
+          <div className="column">
+            <div className="row">
+              <span>PC</span>
+              <span>{this.state.selected?.Pc}</span>
+            </div>
+            <div className="row">
+              <span>Playstation</span>
+              <span>{this.state.selected?.Playstation}</span>
+            </div>
+            <div className="row">
+              <span>Xbox</span>
+              <span>{this.state.selected?.Xbox}</span>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Autocomplete
+            disablePortal
+            autoHighlight
+            id="combo-box-demo"
+            options={this.tricks}
+            sx={{ width: 300 }}
+            onChange={(event, newValue) => {
+              this.handleChange(newValue);
+            }}
+            getOptionLabel={(option: any) => option.move}
+            renderInput={(params) => (
+              <TextField
+                variant="filled"
+                {...params}
+                label="Whats the trick called?"
+              />
+            )}
+          />
+          {view}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
